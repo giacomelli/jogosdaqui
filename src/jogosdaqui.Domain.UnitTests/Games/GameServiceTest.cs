39d28eebdbf164433e0aafde6fde38b1d24a9625
Diego Giacomelli <giacomelli@gmail.com>
2013-05-31 17:43:39 -0300

    Ajustes no NuGet.targets para passar os parâmetros corretos.

---
+++ src/jogosdaqui.Domain.UnitTests/Games/GameServiceTest.cs
@@ -0,0 +1,11 @@
+using NUnit.Framework;
+using System;
+
+namespace jogosdaqui.Domain.UnitTests
+{
+	[TestFixture()]
+	public class GameServiceTest
+	{
+	}
+}
+
---
+++ src/jogosdaqui.Domain.UnitTests/Test.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using System;
+
+namespace jogosdaqui.Domain.UnitTests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
