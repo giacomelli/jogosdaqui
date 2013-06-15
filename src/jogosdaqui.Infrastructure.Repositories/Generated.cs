d29643d07e040f2ec4ffbb023dd563d560246af5
Diego Giacomelli <giacomelli@gmail.com>
2013-06-15 11:04:22 -0300

    Novos geradores de código.

---
+++ src/jogosdaqui.Infrastructure.Repositories/Generated.cs
@@ -0,0 +1,62 @@
+﻿ 
+  
+   
+   
+#region Usings    
+using System;  
+using System.Collections.Generic;    
+using System.IO;       
+using System.Linq;    
+using jogosdaqui.Domain.Games; 
+using Skahal.Infrastructure.Framework.Commons;
+using Skahal.Infrastructure.Framework.Repositories;
+using HelperSharp; 
+using KissSpecifications; 
+#endregion        
+    
+
+   
+
+using System;
+using Skahal.Infrastructure.Framework.Repositories;
+using jogosdaqui.Domain.Games;
+	
+
+namespace jogosdaqui.Infrastructure.Repositories.Testing
+{
+	/// <summary>
+	/// Testing Game repository. 
+	/// </summary>
+	public class TestingGameRepository : MemoryRepository<Game, long>, IGameRepository
+	{
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="jogosdaqui.Infrastructure.Repositories.Testing.TestingGameRepository"/> class.
+		/// </summary>
+		public TestingGameRepository() : base((g) => { return DateTime.Now.Ticks; })
+		{
+		}
+		#endregion
+	}
+}
+
+namespace jogosdaqui.Infrastructure.Repositories.Testing
+{
+	/// <summary>
+	/// Testing GameCategory repository. 
+	/// </summary>
+	public class TestingGameCategoryRepository : MemoryRepository<GameCategory, long>, IGameCategoryRepository
+	{
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="jogosdaqui.Infrastructure.Repositories.Testing.TestingGameCategoryRepository"/> class.
+		/// </summary>
+		public TestingGameCategoryRepository() : base((g) => { return DateTime.Now.Ticks; })
+		{
+		}
+		#endregion
+	}
+}
+ 
---
+++ src/jogosdaqui.WebApi/Generated.cs
@@ -0,0 +1,150 @@
+﻿ 
+  
+   
+   
+
+   
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using AspNetWebApi.ApiGee.Filters;
+using jogosdaqui.Domain;
+using jogosdaqui.Domain.Games;
+	 
+
+namespace jogosdaqui.WebApi.Controllers
+{
+	/// <summary>
+	/// Games.
+	/// </summary>
+    public class GamesController : ApiController
+    {
+		#region Fields
+		private GameService m_service;
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="jogosdaqui.WebApi.Controllers.GamesController"/> class.
+		/// </summary>
+		public GamesController()
+		{
+			m_service = new GameService ();
+		}
+		#endregion
+
+		/// <summary>
+		/// Get all Games
+		/// </summary>
+        public IEnumerable<Game> Get()
+        {
+			return m_service.GetAllGames ();
+        }
+
+		/// <summary>
+		/// Creates a new Game.
+		/// </summary>
+		/// <param name="game">The Game to create.</param>
+		/// <returns>The created Game with the key.</returns>
+		public Game Post(Game game)
+		{
+			m_service.SaveGame (game);
+
+			return game;
+		}
+
+		/// <summary>
+		/// Updates an existing Game.
+		/// </summary>
+		/// <param name="key">The Game's key.</param>
+		/// <param name="game">The Game with updated informations.</param>
+		public Game Put(long key, Game game)
+		{
+	        game.Key = key;
+			m_service.SaveGame (game);
+
+			return game;
+		}
+
+		/// <summary>
+		/// Deletes the Game with the specified key.
+		/// </summary>
+		/// <param name="key">The key of the Game to be deleted.</param>
+	    [SuccessHandlingFilter]
+        public void Delete(long key)
+		{
+			m_service.DeleteGame (key);
+		}
+    }
+}
+
+namespace jogosdaqui.WebApi.Controllers
+{
+	/// <summary>
+	/// GameCategories.
+	/// </summary>
+    public class GameCategoriesController : ApiController
+    {
+		#region Fields
+		private GameCategoryService m_service;
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the <see cref="jogosdaqui.WebApi.Controllers.GameCategoriesController"/> class.
+		/// </summary>
+		public GameCategoriesController()
+		{
+			m_service = new GameCategoryService ();
+		}
+		#endregion
+
+		/// <summary>
+		/// Get all GameCategories
+		/// </summary>
+        public IEnumerable<GameCategory> Get()
+        {
+			return m_service.GetAllGameCategories ();
+        }
+
+		/// <summary>
+		/// Creates a new GameCategory.
+		/// </summary>
+		/// <param name="gamecategory">The GameCategory to create.</param>
+		/// <returns>The created GameCategory with the key.</returns>
+		public GameCategory Post(GameCategory gamecategory)
+		{
+			m_service.SaveGameCategory (gamecategory);
+
+			return gamecategory;
+		}
+
+		/// <summary>
+		/// Updates an existing GameCategory.
+		/// </summary>
+		/// <param name="key">The GameCategory's key.</param>
+		/// <param name="gamecategory">The GameCategory with updated informations.</param>
+		public GameCategory Put(long key, GameCategory gamecategory)
+		{
+	        gamecategory.Key = key;
+			m_service.SaveGameCategory (gamecategory);
+
+			return gamecategory;
+		}
+
+		/// <summary>
+		/// Deletes the GameCategory with the specified key.
+		/// </summary>
+		/// <param name="key">The key of the GameCategory to be deleted.</param>
+	    [SuccessHandlingFilter]
+        public void Delete(long key)
+		{
+			m_service.DeleteGameCategory (key);
+		}
+    }
+}
+ 
