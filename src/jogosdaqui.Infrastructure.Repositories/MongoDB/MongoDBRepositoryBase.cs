0192b2172cc4f3e4c008eb587accec9cb765d24d
Diego Giacomelli <giacomelli@gmail.com>
2013-06-27 07:30:24 -0300

    Criada classe para repositório base de MongoDB.

---
+++ src/jogosdaqui.Infrastructure.Repositories/MongoDB/MongoDBRepositoryBase.cs
@@ -0,0 +1,101 @@
+using System;
+using Skahal.Infrastructure.Framework.Repositories;
+using MongoDB.Driver;
+using System.Configuration;
+using Skahal.Infrastructure.Framework.Domain;
+using MongoDB.Driver.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace jogosdaqui.Infrastructure.Repositories.MongoDB
+{
+	/// <summary>
+	/// Mongo DB repository base class.
+	/// </summary>
+	public abstract class MongoDBRepositoryBase<TEntity>: RepositoryBase<TEntity, long>  
+		where TEntity : IAggregateRoot<long>
+	{
+		#region Fields
+		private MongoCollection<TEntity> m_collection;
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// Initializes a new instance of the
+		/// <see cref="jogosdaqui.Infrastructure.Repositories.MongoDB.MongoDBRepositoryBase`1"/> class.
+		/// </summary>
+		public MongoDBRepositoryBase()
+		{
+			var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
+			var url = new MongoUrl(connectionstring);
+			var client = new MongoClient(url);
+			var server = client.GetServer();
+			var database = server.GetDatabase(url.DatabaseName);
+			m_collection = database.GetCollection<TEntity>("Tags");
+		}
+		#endregion
+
+		#region implemented abstract members of RepositoryBase
+
+		/// <summary>
+		/// Finds the entity by the key.
+		/// </summary>
+		/// <returns>The found entity.</returns>
+		/// <param name="key">Key.</param>
+		public override TEntity FindBy (long key)
+		{
+			return m_collection.AsQueryable<TEntity> ().FirstOrDefault(t => t.Key.Equals(key));
+		}
+
+		/// <summary>
+		/// Finds all entities that matches the filter.
+		/// </summary>
+		/// <returns>The found entities.</returns>
+		/// <param name="offset">Offset.</param>
+		/// <param name="limit">Limit.</param>
+		/// <param name="filter">Filter.</param>
+		public override IEnumerable<TEntity> FindAll (int offset, int limit, Func<TEntity, bool> filter)
+		{
+			return m_collection.FindAll ().Where (filter).Skip (offset).Take (limit);
+		}
+
+		/// <summary>
+		/// Counts all entities that matches the filter.
+		/// </summary>
+		/// <returns>The number of the entities that matches the filter.</returns>
+		/// <param name="filter">Filter.</param>
+		public override long CountAll (Func<TEntity, bool> filter)
+		{
+			return m_collection.FindAll ().LongCount (filter);
+		}
+
+		/// <summary>
+		/// Persists the new item.
+		/// </summary>
+		/// <param name="item">Item.</param>
+		protected override void PersistNewItem (TEntity item)
+		{
+			m_collection.Insert (item);
+		}
+
+		/// <summary>
+		/// Persists the updated item.
+		/// </summary>
+		/// <param name="item">Item.</param>
+		protected override void PersistUpdatedItem (TEntity item)
+		{
+			m_collection.Save (item);
+		}
+
+		/// <summary>
+		/// Persists the deleted item.
+		/// </summary>
+		/// <param name="item">Item.</param>
+		protected override void PersistDeletedItem (TEntity item)
+		{
+			m_collection.Remove(((MongoQueryable<TEntity>) m_collection.AsQueryable<TEntity>().Where(f => f.Key.Equals(item.Key))).GetMongoQuery());			
+		}
+
+		#endregion
+	}
+}
--- src/jogosdaqui.Infrastructure.Repositories/MongoDB/MongoDBTagRepository.cs
+++ src/jogosdaqui.Infrastructure.Repositories/MongoDB/MongoDBTagRepository.cs
@@ -10,64 +10,8 @@ using MongoDB.Driver.Builders;
 
 namespace jogosdaqui.Infrastructure.Repositories.MongoDB
 {
-	public class MongoDBTagRepository : RepositoryBase<Tag, long>,  ITagRepository
+	public class MongoDBTagRepository : MongoDBRepositoryBase<Tag>,  ITagRepository
 	{
-		#region Fields
-		private MongoDatabase m_database;
-		private MongoCollection<Tag> m_collection;
-		#endregion
-
-		#region Constructors
-		public MongoDBTagRepository()
-		{
-			var connectionstring = ConfigurationManager.AppSettings.Get("MONGOLAB_URI");
-			var url = new MongoUrl(connectionstring);
-			var client = new MongoClient(url);
-			var server = client.GetServer();
-			m_database = server.GetDatabase(url.DatabaseName);
-
-			m_collection = m_database.GetCollection<Tag>("Tags");
-			//m_collection.RemoveAll ();
-		}
-		#endregion
-
-		#region implemented abstract members of RepositoryBase
-
-		public override Tag FindBy (long key)
-		{
-			return m_collection.AsQueryable<Tag> ().FirstOrDefault(t => t.Key.Equals(key));
-		}
-
-		public override IEnumerable<Tag> FindAll (int offset, int limit, Func<Tag, bool> filter)
-		{
-			var query = m_collection.AsQueryable<Tag> ().Where(t => true).Skip (offset).Take (limit);
-		
-
-			return query.ToList ();
-		}
-
-		public override long CountAll (Func<Tag, bool> filter)
-		{
-			return m_collection.Count ();
-		}
-
-		protected override void PersistNewItem (Tag item)
-		{
-			m_collection.Insert (item);
-		}
-
-		protected override void PersistUpdatedItem (Tag item)
-		{
-			m_collection.Save (item);
-		}
-
-		protected override void PersistDeletedItem (Tag item)
-		{
-			m_collection.Remove(((MongoQueryable<Tag>) m_collection.AsQueryable<Tag>().Where(f => f.Equals(item))).GetMongoQuery());			
-		}
-
-		#endregion
-
 
 	}
 }
